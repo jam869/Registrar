9dc8adf49ae0a4f9bf06c49cd499d9ff6f2616e3
jam869 <62719891+jam869@users.noreply.github.com>
2026-04-17 10:17:39 -0400

    Add project files.

---
+++ Scripts/SiteScripts.js
@@ -0,0 +1,146 @@
+﻿$(document).ready(function () {
+    $('.phone').mask('(999) 999-9999');
+    $('.phoneExt').mask('(999) 999-9999 poste 99999');
+    $('.zipcode').mask('a9a 9a9');
+    $(".datepicker").datepicker({
+        dateFormat: 'yy-mm-dd',
+        changeMonth: true,
+        changeYear: true,
+        //yearRange: "-100:-15",
+        dayNamesMin: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"],
+        monthNamesShort: ["Janv.", "Févr.", "Mars", "Avril", "Mai", "Juin", "Juil.", "Août", "Sept.", "Oct.", "Nov.", "Déc."]
+    });
+
+    /*Filter unicode hack */
+    $(":input").change(function () {
+        try {
+            let r = $(this).val().replace(/[^\x00-\xFF]/g, "");
+            $(this).val(r);
+        } catch (e) { }
+    });
+    $("textarea").change(function () {
+        try {
+            let r = $(this).val().replace(/[^\x00-\xFF]/g, "");
+            $(this).val(r);
+        } catch (e) { }
+    });
+
+    $(".countrySelect").change((e) => {
+        $(e.target).next().attr("src", "/Images_Data/Loading_icon.gif")
+        $.ajax({
+            url: "/CountryFlag/get?countryCode=" + $(e.target).val(),
+            datatype: "application/json",
+            success: json => { $(e.target).next().attr("src", json); }
+        });
+
+    })
+    SummaryHandling();
+})
+
+
+function SummaryHandling() {
+
+    $('summary').attr('title', 'Utilisez ctrl-clic pour développer/réduire');
+    $('summary').off();
+    // Toggle collapse uncollapse details
+    $('summary').on('click', function (e) {
+        if (e.ctrlKey) {
+            if ($(this).parent().attr('open') != undefined) {
+                $('details').removeAttr('open');
+                e.preventDefault();
+            }
+            else {
+                $('details').prop('open', true);
+                e.preventDefault();
+            }
+        }
+    })
+}
+
+$(".submitCmd").click(function () {
+    $("form").submit();
+})
+function InstallAutoComplete(targetId, words) {
+
+    function split(val) {
+        return val.split(/ \s*/);
+    }
+
+    function RemoveExtra(str, extra) {
+        var extraLength = extra.length;
+        var lastExtraIndex = str.lastIndexOf(extra);
+        if ((lastExtraIndex + extraLength) == str.length)
+            str = str.substring(0, str.length - extraLength);
+        return str;
+    }
+
+    function extractLast(term) {
+        return split(term).pop();
+    }
+
+    $("#" + targetId)
+        // don't navigate away from the field on tab when selecting an item
+        .bind("keydown", function (event) {
+            if (event.keyCode === $.ui.keyCode.TAB && $(this).data("ui-autocomplete").menu.active) {
+                event.preventDefault();
+            }
+        })
+        .autocomplete({
+            minLength: 1,
+            source: function (request, response) {
+                // delegate back to autocomplete, but extract the last term
+                response($.ui.autocomplete.filter(words, extractLast(request.term)));
+            },
+            focus: function () {
+                // prevent value inserted on focus
+                return false;
+            },
+            select: function (event, ui) {
+                var terms = split(this.value);
+                // remove the current input
+                terms.pop();
+                // add the selected item
+                terms.push(ui.item.value);
+                // add placeholder to get the comma-and-space at the end
+                terms.push("");
+                this.value = RemoveExtra(terms, ",").join(" ");
+                return false;
+            }
+        });
+}
+
+function ajaxActionCall(actionLink) {
+    // Ajax Action Call to actionLink
+    $.ajax({
+        url: actionLink,
+        method: 'GET',
+        success: (data) => {
+            console.log("Result: " + data);
+        }
+    });
+}
+function highlight(text, elem) {
+    let innerHTML = elem.innerHTML;
+    let index = innerHTML.toLowerCase().indexOf(text.toLowerCase());
+    if (index >= 0) {
+        innerHTML = innerHTML.substring(0, index) + "<span class='highlight'>" + innerHTML.substring(index, index + text.length) + "</span>" + innerHTML.substring(index + text.length);
+        elem.innerHTML = innerHTML;
+    }
+}
+
+
+function toggleMediaLikeFast(mediaId, btn) {
+    let $icon = $(btn);
+    let $countSpan = $icon.prev('.like-count');
+    let count = parseInt($countSpan.text());
+
+    if ($icon.hasClass('fa-regular')) {
+        $icon.removeClass('fa-regular').addClass('fa-solid');
+        $countSpan.text(count + 1);
+    } else {
+        $icon.removeClass('fa-solid').addClass('fa-regular');
+        $countSpan.text(count - 1);
+    }
+
+    $.get('/Medias/ToggleLike?mediaId=' + mediaId);
+}
